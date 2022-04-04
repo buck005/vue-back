495701fcfef1c4299bddbaa2bfaa5b955932f71f
huxiong <buck005@163.com>
2022-04-04 18:10:52 +0800

    update port

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ let server = http.createServer(function (req, res) {
     res.end("Not Found");
   }
 });
-server.listen(3000, () => {
+server.listen(3001, () => {
   console.log("后端api接口服务器已经启动在3000端口上！");
 });
