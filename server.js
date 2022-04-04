20326e6cf685da3f68525fa264d94d85f95e4cde
huxiong <buck005@163.com>
2022-04-04 23:21:48 +0800

    update

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ let users = [
 ];
 let server = http.createServer(function (req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  if (req.url === "/api1/users") {
+  if (req.url === "/users") {
     res.end(JSON.stringify(users));
   } else {
     res.end("Not Found");
   }
 });
 server.listen(3001, () => {
-  console.log("后端api接口服务器已经启动在3000端口上！");
+  console.log("后端api接口服务器已经启动在3001端口上！");
 });
