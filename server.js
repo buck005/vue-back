fe4c04253dee0ddc4a6c0e0ee97c35b4335d152e
huxiong <buck005@163.com>
2022-04-05 23:29:27 +0800

    update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 let http = require("http");
 let users = [
-  { id: 11, name: "jack" },
-  { id: 22, name: "luck" },
-  { id: 33, name: "king" },
+  { id: 1, name: "jack" },
+  { id: 2, name: "luck" },
+  { id: 3, name: "king" },
 ];
 let server = http.createServer(function (req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
